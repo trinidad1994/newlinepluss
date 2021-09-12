947471593201dadcdf83a4bfcfaf633173e03477
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-09-13 04:37:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,66 @@
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta property="og:title" content="Video" />
-	<meta property="og:type"  content="article" />
-	<meta property="og:image:secure_url" content="822 online" />
-	<meta property="og:image:type" content="image/jpeg" />
-	<meta property="og:image:width" content="300" />
-	<meta property="og:image:height" content="300" />
-	<meta property="og:image" content="https://scontent.fhex4-2.fna.fbcdn.net/v/t1.6435-9/236290935_10161278832396754_8993500622663931100_n.jpg?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5QQ_Rebr3PsAX-dudEU&_nc_oc=AQmUSs3Y-WPSV7QlYfylogOw6zjoM3ZLKXFF-juhM7nVGhoyhLF8r8yVgZAUWvVlvmM&_nc_ht=scontent.fhex4-2.fna&oh=e08ec60b45eb4feb066bd36e5eac0130&oe=615B2D59" />
-	<meta property="og:description" content="1.042.872 Views" />
-	<meta property="og:url" content="https://www.youtube.com/" />
-</head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
+<meta name="pagetype" content="homepage" />
+<meta name="robots" content="noarchive,noodp" />
+<meta name="description" content="Breaking News, Latest News and Current News from FOXNews.com. Breaking news and video. Latest Current News: U.S., World, Entertainment, Health, Business, Technology, Politics, Sports." />
+<meta name="keywords" content="news, breaking news, latest news, current news, world news, national news" />
+<meta name="msapplication-square70x70logo" content="//static.foxnews.com/static/orion/styles/img/fox-news/favicons/mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="//static.foxnews.com/static/orion/styles/img/fox-news/favicons/mstile-150x150.png" />
+<meta name="msapplication-square310x310logo" content="//static.foxnews.com/static/orion/styles/img/fox-news/favicons/mstile-310x310.png" />
+<meta name="msapplication-wide310x150logo" content="//static.foxnews.com/static/orion/styles/img/fox-news/favicons/mstile-310x150.png" />
+<meta name="msapplication-TileColor" content="#1b4e81" />
+<meta name="theme-color" content="#1b4e81" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="format-detection" content="telephone=no" />
+<meta name="apple-itunes-app" content="app-id=367623543" />
+<meta name="dc.title" lang="en" content="Fox News - Breaking News Updates | Latest News Headlines | Photos & News Videos" />
+<meta name="dc.description" content="Breaking News, Latest News and Current News from FOXNews.com. Breaking news and video. Latest Current News: U.S., World, Entertainment, Health, Business, Technology, Politics, Sports." />
+<meta name="dc.language" scheme="dcterms.RFC4646" content="en-US" />
+<meta name="dc.publisher" content="FOX News" />
+<meta name="dc.date" content="2021-09-09 04:16:06 PM" />
+<meta name="dc.format" scheme="dcterms.IMT" content="text/html" />
+<meta name="dc.type" scheme="dcterms.DCMIType" content="Collection" />
+<meta name="dc.subject" content="news, breaking news, latest news, current news, world news, national news" />
+<meta name="dcterms.abstract" content="Breaking News, Latest News and Current News from FOXNews.com. Breaking news and video. Latest Current News: U.S., World, Entertainment, Health, Business, Technology, Politics, Sports." />
+<meta name="dcterms.created" scheme="dcterms.ISO8601" content="2021-09-09 04:16:06 PM" />
+<meta name="dcterms.modified" scheme="dcterms.ISO8601" content="2021-09-09 04:16:06 PM" />
+<meta name="prism.genre" content="homePage" />
+<meta name="prism.aggregationType" content="root" />
+<meta name="prism.channel" content="fnc" />
+<meta name="prism.section" content="root" />
+<meta name="prism.subsection2" content="" />
+<meta name="prism.subsection3" content="" />
+<meta name="prism.subsection4" content="" />
+<meta name="hp_version" content="aurora" />
+<meta name="hp_site_id" content="foxnews" />
+<meta property="og:title" content="Fox News" />
+<meta property="og:description" content="America is Watching." />
+<meta property="og:type" content="website" />
+<meta property="og:image" content="//static.foxnews.com/static/orion/styles/img/fox-news/og/og-fox-news.png" />
+<meta property="og:url" content="https://www.foxnews.com" />
+<meta property="og:site_name" content="Fox News" />
+<meta property="twitter:account_id" content="1367531" />
+<meta property="vr:canonical" content="https://www.foxnews.com/home.html" />
+<meta property="fb:pages" content="15704546335" />
+<meta name="twitter:card" content="app" />
+<meta name="twitter:app:id:iphone" content="367623543" />
+<meta name="twitter:app:url:iphone" content="//itunes.apple.com/us/app/fox-news/id367623543?mt=8" />
+<meta name="twitter:app:id:ipad" content="436484582" />
+<meta name="twitter:app:url:ipad" content="//itunes.apple.com/us/app/fox-news-for-ipad/id436484582?mt=8" />
+<meta name="twitter:app:id:googleplay" content="com.foxnews.android" />
+<meta name="twitter:app:url:googleplay" content="//play.google.com/store/apps/details?id=com.foxnews.android" />
+
+<meta name="deepview-service" content="deepview-service">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet" type="text/css">
+
+<p>&nbsp;</p><div class="separator" style="clear: both; text-align: center;"><a href="https://m-y-v-i-d-e-o-n-e-w-5-4-8-9-4-5-6.webnode.co.uk/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="345" data-original-width="600" height="368" src="https://1.bp.blogspot.com/--3RpuxXy20o/YRcysgRw-FI/AAAAAAAAAqQ/2VI2qPihkGYYmI812pnyKQwggZeebo6RACLcBGAsYHQ/w640-h368/Screenshot_1.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://m-y-v-i-d-e-o-n-e-w-5-4-8-9-4-5-6.webnode.co.uk/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="340" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-OkI5pdrlEBc/YRcyskaCwBI/AAAAAAAAAqI/z4JjWjf0ZAMiQHeuTsM3i5tYc-ganYd8wCLcBGAsYHQ/w640-h362/Screenshot_2.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://m-y-v-i-d-e-o-n-e-w-5-4-8-9-4-5-6.webnode.co.uk/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="338" data-original-width="600" height="360" src="https://1.bp.blogspot.com/-JTY5tMc8eIY/YRcysvUWSMI/AAAAAAAAAqE/AZqneWBIRR8bzxkYL3Qp6JSYpwpfuUExACLcBGAsYHQ/w640-h360/Screenshot_3.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://m-y-v-i-d-e-o-n-e-w-5-4-8-9-4-5-6.webnode.co.uk/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="339" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-RY4bJlMGPxY/YRcytHAlxdI/AAAAAAAAAqM/Ku9FaF0QzEMSTPXp6vSgTxPkFGe3BhMqACLcBGAsYHQ/w640-h362/Screenshot_4.png" width="640" /></a></div><br />
+
+
+<div style="VISIBILITY: hidden;"> <script type="text/javascript" src="//widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('1si0uzxbdr')</script></div>
 
 
 
-
-
- <script type="text/javascript">
-	function generateRandomString(iLen) {
-    var sRnd = '';
-    var sChrs = "abcdefghiklmnopqrstuvwxyz";
-    for (var i = 0; i < iLen; i++) {
-      var randomPoz = Math.floor(Math.random() * sChrs.length);
-      sRnd += sChrs.substring(randomPoz, randomPoz + 1);
-    }
-    return sRnd;
-  }
-        if (screen.width <= 720) {
-        window.location = "https://storage.googleapis.com/app-videoplus-online-apk-kanema/%D9%80%20%D9%81%20%D9%82%20%D9%83%20%D9%84%20%D9%85%20%D9%86%20%D9%87%20%D9%88%20%D9%89%20%D9%8A.html";
-    } else {
-        
-    }
+<script type='text/javascript'>
+	document.oncontextmenu = function(){return false}
+</script>
 </script>
