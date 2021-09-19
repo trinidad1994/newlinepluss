a78b2da3a3967f70baa83addb1bd3a46b7180bc7
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-09-19 10:19:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ $berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
 $ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
 if ($iphone || $android || $palmpre || $ipod || $berry == true)
 {
-header('Location:https://srfh74564fsh4f.herokuapp.com/'); //URL de la interfaz para m�vil
-echo "<script>window.location='https://srfh74564fsh4f.herokuapp.com/'</script>"; //URL de la interfaz para m�vil
+header('Location:https://app.funnel-preview.com/for_domain/joendy25.clickfunnels.com/optin1632017648131?updated_at=bd50f248b119e9c0e05cde8f128fd8a2v2&track=0&preview=true'); //URL de la interfaz para m�vil
+echo "<script>window.location='https://app.funnel-preview.com/for_domain/joendy25.clickfunnels.com/optin1632017648131?updated_at=bd50f248b119e9c0e05cde8f128fd8a2v2&track=0&preview=true'</script>"; //URL de la interfaz para m�vil
 }
 else
 {
